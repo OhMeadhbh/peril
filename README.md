a58aaa7b7f41cd95db59368c82d88cc0a101dba7
Meadhbh Hamrick <OhMeadhbh@gmail.com>
2015-02-02 16:53:44 -0800

    Late Pre-Function Complete, but the API is stableish.

--- README.md
+++ README.md
@@ -1,2 +1,181 @@
 # peril
-Create and manage profiles containing files in /etc
+Create and manage profiles containing files in /etc with sym-links.
+
+## Wait. What?
+
+"Peril" is a tool that creates adminstrative 'profiles' for *nix type
+systems. Many of our embedded projects have a "recovery" profile that
+starts up DHCP and HostAPD with a predefined WPA password. But for
+normal operation we use the "nominal" profile with end-user defined
+network and user settings.
+
+"Peril" keeps the two sets of /etc files segregated by putting them in
+different profiles. Each profile has a directory in /etc/peril that
+profile's "real" files live. Instead of having "real" files in /etc,
+we use sym-links.
+
+So... for instance, let's say we wanted to have two profiles: recovery
+and nominal. In the recovery profile you want no default user accounts
+and to make sure HostAPD starts with a pre-configured WPA password. In
+the nominal account you want to let the user configure their own
+network settings and user accounts. Rather than having scripts that
+copy default files and make backups when you enter each mode, we
+create soft links from the "important" files like /etc/passwd,
+/etc/hostapd.conf, etc. that point to the "real" files in
+/etc/peril/<profile name>/<whatever>.
+
+So for this example, our /etc directory would look something like this
+in recovery mode:
+
+    /etc/peril/current --symlink to--> /etc/peril/recovery
+    /etc/passwd        --------------> /etc/peril/current/etc/passwd
+    /etc/group         --------------> /etc/peril/current/etc/group
+    /etc/rc?.d         --------------> /etc/peril/current/etc/rc?.d
+    /etc/rc.local      --------------> /etc/peril/current/etc/rc.local
+    
+    /etc/peril/recovery/etc/passwd   <--- nominal profile's password file
+    /etc/peril/recovery/etc/group    <--- nominal profile's groups file
+    /etc/peril/recovery/etc/rc?.d    <--- contains links to nominal mode's init scripts
+    /etc/peril/recovery/etc/rc.local <--- nominal mode's rc.local file
+
+And then nominal mode would be the same, except the /etc/peril/current
+link would point to the /etc/peril/nominal directory, which would
+contain the "real" files for nominal mode.
+
+This is a hack. But after trying to do the "right" thing by using
+version control systems to hold config files I discovered that
+sometimes hacks work better than doing things the "right" way.
+
+Peril does not do version control. But it doesn't get in your way if
+you want to put /etc/peril under version control. Please note,
+however, most version control systems (*cough* *git* *cough*) don't
+store user metadata, so you have to do post checkout user fixups
+manually.
+
+Peril can also make your system unusable if it fails in the middle of
+a critical transaction. Please don't issue peril commands as your
+system is rebooting.
+
+## GURF! YOU'RE MODIFYING /etc ON THE FLY?
+
+Well. Yes and no. You *can* issue peril commands that take effect
+immediately (like adding or removing a file from a profile.) This is
+probably a bad idea only if some process has the file open at the time
+you add (or remove) it from the profile. It's not 100% safe, but it is
+99% safe.
+
+But doing things like changing the targets of various sym-links while
+processes that depend on them may be a bad idea. So, by default, when
+you tell peril to change the profile, it doesn't change the current
+profile, it changes the "next" profile. "Next" as in "use this profile
+the next time i reboot."
+
+If you feel lucky, you can tell peril to make the current profile the
+one pointed to by the next profile pointer.
+
+## Profile Pointer, WTF?
+
+Let's recap: systems have (potentially) multiple profiles. Each
+profile may have different versions of the same file in it. We use
+symlinks to link a file (like /etc/passwd) to a real file with real
+contents in /etc/peril/<profile name> (like
+/etc/peril/nominal/etc/passwd.)
+
+But if you look at the profile description above, you'll notice that
+the symlink we put at /etc/passwd doesn't point to
+/etc/peril/nominal/etc/passwd, it points to
+/etc/peril/current/etc/passwd. Then if you were able to look at the
+rest of that list without falling asleep you might have noticed
+/etc/peril/current points to the "real" profile directory.
+
+By default, /etc/peril/current and /etc/peril/next are symbolic links
+pointing to real profile directories. Operations that modify the
+contents of a profile, like adding/removing a file, occur on the
+"next" profile. Once you've made all the changes you want to make, you
+change the "current" profile to point to the same profile as the
+"next" profile.
+
+Or, if you feel lucky, you can make the next pointer point to the
+current pointer and hope that everything works out.
+
+The way we do "recovery mode" in our systems is to check whether the
+"recovery condition" is true at boot time (usually this means the
+recovery button is being pressed at boot time, or the RECOVERY.TXT
+file exists in the boot partition.) If so, we change the
+/etc/peril/next to point to /etc/peril/nominal and /etc/peril/current
+to point to /etc/peril/recovery. This causes the system to boot into
+recovery mode (because the current profile pointer is pointing at
+/etc/peril/recovery.)
+
+## Doing Things with Peril
+
+There are three classes of objects peril manipulates: profiles,
+profile pointers and files. Profiles hold collections of
+administrative files. Profiles are, in turn, linked to profile
+pointers.
+
+To list profiles, use the `peril profile list` command:
+
+    *$* peril profile list
+    recovery
+    factory
+    nominal
+
+To create or delete a profile, use the `peril profile create <profile
+name>` or `peril profile delete <profile name>` command:
+
+    *$* peril profile create example
+    *$* echo $?
+    0
+    *$* peril profile delete example
+
+To add a file to a profile, use the `peril file add <file name>
+[<profile name>]` command:
+
+    *$* sudo peril file add /etc/passwd
+    *$* echo $?
+    0
+
+If you don't include a profile name in the file add command, peril
+will copy the existing file to all the profiles. If you add a file
+that's already in a profile, peril will emit an error message on
+stderr and give a return code of 2:
+
+    *$* sudo peril file add /etc/passwd
+    peril: file /etc/passwd is already in this profile
+    *$* echo $?
+    2
+
+If you include a profile name in the file add command, it will only
+copy the file to THAT profile:
+
+    *$* sudo peril file add /etc/hostapd/hostapd.conf recovery
+    *$* echo $?
+    0
+
+To remove a file from peril, use the `peril file rm <file name> [<profile name>]`
+command:
+
+    *$* sudo peril file rm /etc/hostapd/hostapd.conf
+    *$* echo $?
+    0
+
+If you do not provide a profile name to the file rm command, it will
+remove that file from all profiles and replace the peril-managed
+symlink with the version of the file from the current profile.  If you
+provide a profile name, it will only remove the file from THAT
+profile. The original file will still be a symlink.
+
+To get the path to the "real" file for a particular file, use the
+`peril file ref <file name> [<profile name>]` command:
+
+    *$* peril file ref /etc/passwd
+    /etc/peril/nominal/etc/passwd
+
+To get the path to a "real" file from a particular profile, do
+something like this:
+
+    *$* peril file ref /etc/passwd recovery
+    /etc/peril/recovery/etc/passwd
+
+But that shouldn't be too shocking.
---
+++ peril.js
@@ -0,0 +1,287 @@
+( function () {
+
+              require( 'sn-core' );
+  var fs    = require( 'fs' );
+  var spawn = require( 'child_process' ).spawn;
+
+  _$construct( 'exports', { _root: [], ignore: [] }, module );
+
+  module.exports.prototype._$init = function () {
+    if( this.root ) {
+      if( '/' === this.root.substr( -1, 1 ) ) {
+        this.root = this.root.slice( 0, -1 );
+      }
+      this._root = this.root.split( '/' );
+    }
+  };
+
+  module.exports.prototype._$initAsync = function ( callback ) {
+    callback = _$g( callback );
+    try {
+      fs.readFile( this._root.concat( [ 'etc', 'peril', 'peril.json' ] ).join( '/' ), function( err, data ) {
+        if( err && ( 'ENOENT' !== err.code ) ) { return callback( err ); }
+        if( ! err ) { this._$merge( JSON.parse( data.toString() ) ); }
+        callback( null, this );
+      }.bind( this ) );
+    } catch( e ) {
+      callback( e );
+    }
+  };
+
+  module.exports.prototype.profile_list = function ( callback ) {
+    callback = _$g( callback );
+    var profiles = [];
+    fs.readdir( this._root.concat( [ 'etc', 'peril' ] ).join( '/' ), function( err, data ) {
+      if( err ) { return callback( err ); }
+      __process_dirs.call( this, data );
+    }.bind( this ) );
+
+    function __process_dirs( list ) {
+      if( 0 == list.length ) {
+        return callback( null, profiles.join( '\n' ) );
+      }
+      var current = list.shift();
+      fs.lstat( this._root.concat( [ 'etc', 'peril', current ] ).join( '/' ), function( err, stats ) {
+        if( ( ! err ) && stats.isDirectory() &&
+            ( ! this.ignore._$any( function( e ) { return current === e; } ) ) ) {
+          profiles.push( current );
+        }
+        (__process_dirs.bind(this))._$nextTick( list );
+      }.bind( this ) );
+    }
+  };
+
+  module.exports.prototype.profile_create = function ( name, callback ) {
+    callback = _$g( callback );
+    var path = this._root.concat( [ 'etc', 'peril', name ] ).join( '/' );
+    fs.lstat( path, function( err, stats ) {
+      if( err && err.code !== 'ENOENT' ) { return callback( err ); }
+      if( ! err ) {
+        return callback();
+      }
+      fs.mkdir( path, function( err ) {
+        if( err ) { return callback( err ); }
+        return callback();
+      } );
+    } );
+  };
+
+  module.exports.prototype.profile_destroy = function ( name, callback ) {
+    callback = _$g( callback );
+    if( ! name ) { return callback(); }
+    var path = this._root.concat( [ 'etc', 'peril', name ] ).join( '/' );
+    fs.lstat( path, function( err, stats ) {
+      if( err && err.code !== 'ENOENT' ) { return callback( err ); }
+      if( err ) {
+        return callback();
+      }
+      console.log( 'rm -rf ' + path );
+      var rm = spawn( 'rm', [ '-rf', path ] );
+      rm.on( 'close', function( code ) {
+        var err;
+        if( 0 !== code ) {
+          err = new Error( 'rm command returned with non-zero return code: ' + code );
+        }
+        callback( err );
+      } );
+    } );
+  };
+
+  module.exports.prototype.profile_ref = function ( name, callback ) {
+    callback = _$g( callback );
+    if( ! name ) { return callback(); }
+    var path = this._root.concat( [ 'etc', 'peril', name ] ).join( '/' ); 
+    fs.lstat( path, function( err, stats ) {
+      if( err ) { return callback(); }
+      if( stats.isDirectory() &&
+          ( ! this.ignore._$any( function( e ) { return name === e; } ) ) ) {
+        return callback( null, path );
+      } 
+      return callback();
+    }.bind( this ) );
+  };
+
+  module.exports.prototype.pointer_list = function ( callback ) {
+    callback = _$g( callback );
+    var profiles = [];
+    fs.readdir( this._root.concat( [ 'etc', 'peril' ] ).join( '/' ), function( err, data ) {
+      if( err ) { return callback( err ); }
+      __process_dirs.call( this, data );
+    }.bind( this ) );
+
+    function __process_dirs( list ) {
+      if( 0 == list.length ) {
+        return callback( null, profiles.join( '\n' ) );
+      }
+      var current = list.shift();
+      fs.lstat( this._root.concat( [ 'etc', 'peril', current ] ).join( '/' ), function( err, stats ) {
+        if( ( ! err ) && stats.isSymbolicLink() &&
+            ( ! this.ignore._$any( function( e ) { return current === e; } ) ) ) {
+          profiles.push( current );
+        }
+        (__process_dirs.bind(this))._$nextTick( list );
+      }.bind( this ) );
+    }
+  };
+
+  module.exports.prototype.pointer_link = function ( pointer, profile, callback) {
+    callback = _$g( callback );
+    if( ( ! pointer ) || ( ! profile ) ) {
+      return callback();
+    }
+
+    var path = this._root.concat( [ 'etc', 'peril', pointer ] ).join( '/' ); 
+    // if the link already exists, delete it.
+    fs.lstat( path, function( err, stats ) {
+      if( err && ( 'ENOENT' !== err.code ) ) { return callback( err ); }
+      if( ! err ) {
+        if( stats.isSymbolicLink() ) {
+          return __remove_old_link();
+        } else {
+          return callback( new Error( pointer + " is not a pointer" ) );
+        }
+      } else {
+        return __make_new_link();
+      }
+    } );
+
+    function __remove_old_link() {
+      fs.unlink( path, function( err ) {
+        if( err ) { return callback( err ); }
+        return __make_new_link();
+      } );
+    }
+
+    function __make_new_link() {
+      fs.symlink( [ '.', profile ].join( '/' ), path, function( err ) {
+        if( err ) { return callback( err ); }
+        return callback();
+      } );
+    }
+              
+  };
+
+  module.exports.prototype.pointer_unlink = function ( pointer, callback ) {
+    callback = _$g( callback );
+    if( ! pointer ) {
+      return callback();
+    }
+
+    var path = this._root.concat( [ 'etc', 'peril', pointer ] ).join( '/' ); 
+
+    fs.lstat( path, function( err, stats ) {
+      if( err ) { return callback( err ); }
+
+      if( stats.isSymbolicLink() ) {
+        fs.unlink( path, function( err ) {
+          if( err ) { return callback( err ); }
+          return callback();
+        } );
+      } else {
+        return callback( new Error( pointer + " is not a pointer" ) );
+      }
+    } );
+  };
+
+  module.exports.prototype.pointer_ref = function ( name, callback ) {
+    callback = _$g( callback );
+    if( ! name ) { return callback(); }
+    var path = this._root.concat( [ 'etc', 'peril', name ] ).join( '/' ); 
+    fs.lstat( path, function( err, stats ) {
+      if( err ) { return callback(); }
+      if( stats.isSymbolicLink() &&
+          ( ! this.ignore._$any( function( e ) { return name === e; } ) ) ) {
+        return callback( null, path );
+      } 
+      return callback();
+    }.bind( this ) );
+  };
+
+  module.exports.prototype.file_add = function ( path, callback ) { 
+    callback = _$g( callback );
+    var confpath = this._root.concat( [ 'etc', 'peril' ] ).join( '/' ); 
+    var sourcepath = (path.substr(0,1)=='/')?path:[process.cwd(),path].join('/');
+    var statdata;
+
+    fs.lstat( path, function( err, data ) {
+      if( err ) { return callback( err ); }
+      statdata = data;
+      __get_profile_paths.call( this, function( err, paths ) {
+        if( err ) { return callback( err ); }
+        return __copy_to_path.call( this, paths );
+      }.bind(this) );
+    }.bind(this) );
+
+    function _build_final_link() {
+//      fs.unlink( sour );
+      console.log( "ln -s " + sourcepath );
+      callback();
+    }
+
+    function __copy_to_path( paths ) {
+      if( ( ! paths ) || ( 0 == paths.length ) ) { return _build_final_link(); }
+      var current = paths.shift();
+      var currentpath = [ confpath, current ].join( '/' ) + sourcepath;
+      var currentdir = currentpath.split( '/' ).slice(0,-1).join('/');
+      fs.stat( currentdir, function( err, data ) {
+        if( err && ( 'ENOENT' !== err ) ) { return callback( err ); }
+        if( data && ( ! data.isDirectory() ) ) { return callback( new Error( 'eep' ) ); }
+        if( err ) {
+          var mkdir = spawn( 'mkdir', [ '-p', currentdir ] );
+          mkdir.on( 'close', function( code ) {
+            if( 0 !== code ) {
+              return callback( new Error( 'mkdir command returned non-zero code: ' + code ) );
+            }
+            return __really_copy_it_this_time();
+          } );
+        }
+        __really_copy_it_this_time();
+      } );
+
+      function __really_copy_it_this_time() {
+        var cp;
+        if( statdata.isDirectory() ) {
+          cp = spawn( 'cp', [ '-r', sourcepath, currentpath ] );
+          console.log( 'cp -r ' + sourcepath + ' ' + currentpath );
+        } else {
+          cp = spawn( 'cp', [ '-P', sourcepath, currentpath ] );
+          console.log( 'cp -P ' + sourcepath + ' ' + currentpath );
+        }
+        cp.on( 'close', function( code ) {
+          if( 0 !== code ) {
+            return callback( new Error( 'cp command returned non-zero return code: ' + code ) );
+          } else {
+            return __copy_to_path._$nextTick( paths );
+          }
+        } );
+      }
+    }
+
+    function __get_profile_paths( complete ) {
+      var paths = [];
+      fs.readdir( confpath, function( err, data ) {
+        if( err ) { return complete( err ); }
+        return __test_next_path( data );
+      } );
+
+      function __test_next_path( data ) {
+        if( ( ! data ) || ( 0 == data.length ) ) { return complete( null, paths ); }
+        var current = data.shift();
+        fs.lstat( [ confpath, current ].join( '/' ), function( err, stats ) {
+          if( err ) { return complete( err ); }
+          if( stats.isDirectory() ) { paths.push( current ); }
+          __test_next_path._$nextTick( data );
+        } );
+      }
+    }
+  };
+
+  module.exports.prototype.file_remove = function ( path, callback ) {
+    return 'file remove' + this.root;
+  };
+
+  module.exports.prototype.file_ref = function ( path, callback ) {
+    return 'file ref' + this.root;
+  };
+
+} ) ();
